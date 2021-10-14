7402c5ea3a210590fa498d028dc0abb9199fc93a
xbrucelee88x <junkemail88@aol.com>
2021-10-14 16:14:38 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
 
-asfdasdf
-3
-asdfasfdaf
+#https://github.com/alpacahq/alpaca-trade-api-python
+
+from alpaca_trade_api.rest import REST
+api = REST()
+
+api.get_bars("AAPL", TimeFrame.Hour, "2021-06-08", "2021-06-08", adjustment='raw').df
+
